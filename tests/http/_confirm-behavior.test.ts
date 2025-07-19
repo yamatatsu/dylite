9badf356e3d3f2610af9be02530f48aaea97810d
yamatatsu <the.kinnikuman@gmail.com>
2025-07-19 16:27:10 +0900

    test: update SET Action tests for operand function usage

--- tests/http/_confirm-behavior.test.ts
+++ tests/http/_confirm-behavior.test.ts
@@ -1,16 +1,33 @@
-import { value } from "valibot";
 import { PkTable, ddb, toUint8Array } from "./_test-helper";
 
 describe.skip("Update Expressions", () => {
 	let tableName: string;
 	const item = PkTable.getItem1();
 
-	beforeAll(async () => {
+	beforeEach(async () => {
 		const table = await PkTable.create();
 		tableName = table.tableName;
 		await ddb.putItem({
 			TableName: tableName,
 			Item: item,
+		});
+	});
+
+	describe("SET Action", () => {
+		test("use a function as an operand of '+' operator", async () => {
+			// WHEN
+			const res = await ddb.updateItem({
+				TableName: tableName,
+				Key: { pk: item.pk },
+				UpdateExpression:
+					"SET key_N = key_N + if_not_exists(key_not_exists, :n)",
+				ExpressionAttributeValues: { ":n": { N: "1" } },
+				ReturnValues: "ALL_NEW",
+			});
+
+			// THEN
+			expect(res.Attributes?.key_N).toEqual({ N: "124" });
+			expect(res.$metadata.httpStatusCode).toBe(200);
 		});
 	});
 
