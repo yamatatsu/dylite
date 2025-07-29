8fb1e881d859fa54b794a21527fd81ae3e475286
yamatatsu <the.kinnikuman@gmail.com>
2025-07-30 08:52:42 +0900

    feat: add resourceInUseException for handling resource in use errors

--- lib/domain/errors.ts
+++ lib/domain/errors.ts
@@ -3,3 +3,9 @@ export function validationException(msg: string) {
 	err.name = "com.amazon.coral.validate#ValidationException";
 	return err;
 }
+
+export function resourceInUseException(msg: string) {
+	const err = new Error(msg);
+	err.name = "com.amazonaws.dynamodb.v20120810#ResourceInUseException";
+	return err;
+}
