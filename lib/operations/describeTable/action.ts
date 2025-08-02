1df1b9fd1380a1c1b0d8b8460c881705aa1b2e79
yamatatsu <the.kinnikuman@gmail.com>
2025-08-02 22:10:59 +0900

    chore: remove unused table operation files

--- lib/operations/describeTable/action.ts
+++
@@ -1,8 +0,0 @@
-import type { Store } from "../../db/types";
-import type { Schema } from "./schema";
-
-export async function action(store: Store, data: Schema) {
-	const tableStore = store.tableStore;
-	const table = await tableStore.get(data.TableName);
-	return { Table: table };
-}
